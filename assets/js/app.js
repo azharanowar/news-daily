ca7107509e2e10692a1f23c692d8f72c836bcc47
azharanowar <azharanowar@gmail.com>
2022-10-02 19:10:01 +0600

    Website preloader and no data found error message function created

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const loadAllCategories = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    if (data.status) {
+        displayAllCategories(data.data);
+    } else {
+        // Error data not found...
+        dataNotFoundMessageSection(true);
+        websitePreloader(false);
+    }
+}
+
+const displayAllCategories = categoriesData => {
+    const newsCategories = categoriesData.news_category;
+    const mainMenuUl = document.getElementById("mainMenuUl");
+    newsCategories.forEach(newsCategory => {
+        const newNavItem = document.createElement('li');
+        newNavItem.classList.add('nav-item');
+        newNavItem.innerHTML = `<a class="nav-link" onclick="getAllNewsByCategoryId('${newsCategory.category_id}')">${newsCategory.category_name}</a>`;
+        mainMenuUl.appendChild(newNavItem);
+    })
+}
+
+loadAllCategories()
+
+const websitePreloader = isActive => {
+    const preloaderSection = document.getElementById("preloaderSection");
+    if (isActive) {
+        preloaderSection.style.display = "block";
+    } else {
+        preloaderSection.style.display = "none";
+    }
+}
+
+const dataNotFoundMessageSection = noDataFound => {
+    const noDataFoundSection = document.getElementById("noDataFoundSection");
+    if (noDataFound) {
+        noDataFoundSection.style.display = "block";
+    } else {
+        noDataFoundSection.style.display = "none";
+    }
+}
