3666b21b55b8562f2093965ed4a7c43952ee5d31
azharanowar <azharanowar@gmail.com>
2022-10-02 19:12:09 +0600

    Load all categories data from API and display all news categories to main menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
+const loadAllCategories = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    if (data.status) {
+        displayAllCategories(data.data);
+    } else {
+        // Error data not found...
+        dataNotFoundMessageSection(true);
+        websitePreloader(false);
+    }
+}
+
+const displayAllCategories = categoriesData => {
+    const newsCategories = categoriesData.news_category;
+    const mainMenuUl = document.getElementById("mainMenuUl");
+    newsCategories.forEach(newsCategory => {
+        const newNavItem = document.createElement('li');
+        newNavItem.classList.add('nav-item');
+        newNavItem.innerHTML = `<a class="nav-link" onclick="getAllNewsByCategoryId('${newsCategory.category_id}')">${newsCategory.category_name}</a>`;
+        mainMenuUl.appendChild(newNavItem);
+    })
+}
+
+loadAllCategories()
+
+
 const websitePreloader = isActive => {
     const preloaderSection = document.getElementById("preloaderSection");
     if (isActive) {
